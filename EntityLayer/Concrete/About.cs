44298a4eafb20ac3ddb3c008fe171d84acdcf2c2
gkcayci <gokkay97@gmaik.com>
2021-09-14 15:34:31 +0300

    migration işlemleri ve generic repository

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
+   
     public class About
     {
+        [Key]
         public int AboutID { get; set; }
+
         public string AboutDeatils1 { get; set; }
         public string AboutDeatils2 { get; set; }
         public string AboutImage1 { get; set; }
